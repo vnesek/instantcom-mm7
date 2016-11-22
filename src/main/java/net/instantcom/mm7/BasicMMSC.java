1d9c61146fd6e4f5594ba1f2552b2a8b75e8f004
bardug <ami.berdugo@gmail.com>
2016-11-22 14:53:14 +0200

    - add a new MMSC implementation for secure connection accepting all x509 certificates
    - add missing headers for outgoing binary content mime part and make base64 default content type instead of 8bit
    
    - read missing data of DeliverReq and add a save method for printing it out

--- src/main/java/net/instantcom/mm7/BasicMMSC.java
+++ src/main/java/net/instantcom/mm7/BasicMMSC.java
@@ -18,10 +18,7 @@
 
 package net.instantcom.mm7;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -30,105 +27,14 @@ import java.net.URL;
  * to MMSC. Use {@link MM7Context} to configure message
  * serialization/deserialization.
  */
-public class BasicMMSC implements MMSC {
+public class BasicMMSC extends MMSCBase {
 
 	public BasicMMSC(String url) {
-		setUrl(url);
+		super(url);
 	}
 
 	@Override
-	public SubmitRsp submit(SubmitReq submitReq) throws MM7Error {
-		MM7Response rsp = post(submitReq);
-		if (!(rsp instanceof SubmitRsp)) {
-			// Error
-			throw new MM7Error(rsp.toString());
-		}
-		return (SubmitRsp) rsp;
+	protected HttpURLConnection getHttpURLConnection(URL u) throws IOException {
+		return (HttpURLConnection) u.openConnection();
 	}
-
-	private MM7Response post(MM7Request request) throws MM7Error {
-		try {
-			final MM7Context ctx = getContext();
-			final URL u = new URL(getUrl());
-			final HttpURLConnection conn = (HttpURLConnection) u.openConnection();
-
-			conn.setDoInput(true);
-			conn.setDoOutput(true);
-			conn.setUseCaches(false);
-
-			conn.setRequestProperty("Content-Type", request.getSoapContentType());
-			conn.setRequestProperty("User-Agent", ctx.getUserAgent());
-			conn.setRequestProperty("Accept", "*/*");
-			conn.setRequestProperty("SOAPAction", "\"\"");
-
-			// HTTP Basic authorization
-			if (ctx.getUsername() != null) {
-				String authString = ctx.getUsername() + ':' + ctx.getPassword();
-				try {
-					ByteArrayOutputStream buffer = new ByteArrayOutputStream(128);
-					OutputStream buffer64 = ctx.newBase64OutputStream(buffer);
-					buffer64.write(authString.getBytes("iso-8859-1"));
-					buffer64.close();
-					conn.setRequestProperty("Authorization", "Basic " + buffer.toString("iso-8859-1"));
-				} catch (IOException ioe) {
-					throw new RuntimeException("Failed to add HTTP Basic Authorization header", ioe);
-				}
-			}
-
-			final OutputStream out = conn.getOutputStream();
-			try {
-				MM7Message.save(request, out, ctx);
-			} finally {
-				out.close();
-			}
-
-			String contentType = conn.getContentType();
-			try {
-				if (contentType.startsWith("text/xml") || contentType.startsWith("multipart/")) {
-					InputStream in;
-					try {
-						in = conn.getInputStream();
-					} catch (IOException e) {
-						if (conn.getResponseCode() >= 400) {
-							in = conn.getErrorStream();
-						} else {
-							throw e;
-						}
-					}
-					return (MM7Response) MM7Message.load(in, contentType, getContext());
-				} else {
-					throw new MM7Error("unexpected content type: " + contentType + ", status: " + //
-							conn.getResponseCode() + " " + conn.getResponseMessage() + ", content: " + //
-							conn.getContent());
-				}
-			} finally {
-				conn.disconnect();
-			}
-		} catch (IOException ioe) {
-			throw new MM7Error("IO error: " + ioe.getMessage());
-		}
-	}
-
-	public String getUrl() {
-		return url;
-	}
-
-	public void setUrl(String url) {
-		this.url = url;
-	}
-
-	public void setContext(MM7Context context) {
-		this.context = context;
-	}
-
-	@Override
-	public MM7Context getContext() {
-		if (context == null) {
-			context = new MM7Context();
-		}
-		return context;
-	}
-
-	private String url;
-	private MM7Context context;
 }
