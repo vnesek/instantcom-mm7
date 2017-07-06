35c0d95a97962733a68a7ba25733c1e238edf5c1
Funzinator <github@funzi.de>
2017-07-06 09:42:32 +0200

    remove tests for com.sun.xml.internal
    using com.sun.xml.internal.* does not work with non-Oracle JVMs.
    Removing test for this one.
    
    As the changes only affect common-codec, it can be safely omited.

--- src/test/java/net/instantcom/mm7/Base64EncoderTest.java
+++ src/test/java/net/instantcom/mm7/Base64EncoderTest.java
@@ -21,8 +21,6 @@ package net.instantcom.mm7;
 import org.apache.commons.codec.binary.Base64OutputStream;
 import org.junit.Test;
 
-import com.sun.xml.internal.messaging.saaj.packaging.mime.util.BASE64EncoderStream;
-
 /**
  * Test Base64 encoder construction
  */
@@ -32,11 +30,5 @@ public class Base64EncoderTest {
 	public void base64ApacheCommons() {
 		MMSC mmsc = new BasicMMSC("http://localhost:8080");
 		mmsc.getContext().setBase64OutputStream(Base64OutputStream.class);
-	}
-
-	@Test
-	public void base64SunSAAJ() {
-		MMSC mmsc = new BasicMMSC("http://localhost:8080");
-		mmsc.getContext().setBase64OutputStream(BASE64EncoderStream.class);
 	}
 }
